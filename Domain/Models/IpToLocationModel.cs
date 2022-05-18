5cb89f2fca9664ae3fa3b553236da71bf8a29ab3
Germán <germanmoreyra94@gmail.com>
2022-05-18 19:47:00 -0300

    Add mapping test

--- Domain/Models/IpToLocationModel.cs
+++ Domain/Models/IpToLocationModel.cs
@@ -12,15 +12,16 @@ namespace Domain.Models
         public string Longitude { get; set; }
         public List<CurrencyModel> Currencies { get; set; }
         public List<string> CurrenciesDollarValue { get; set; }
-
         public class CurrencyModel
         {
             public string Code { get; set; }
         }
 
+        private readonly string _missingData = "Missing information";
+
         public List<string> GetCurrenciesList()
         {
-            if (Currencies == null)
+            if (Currencies == null || Currencies.Count == 0)
             {
                 return new List<string>();
             }
@@ -33,6 +34,11 @@ namespace Domain.Models
 
         public string GetDistance()
         {
+            if (string.IsNullOrWhiteSpace(Latitude) || string.IsNullOrWhiteSpace(Longitude))
+            {
+                return _missingData;
+            }
+
             return $"{DistanceUtils.DistanceToBA(Latitude, Longitude)} KMs";
         }
 
@@ -41,6 +47,11 @@ namespace Domain.Models
             var currency = string.Empty;
             var index = 0;
             var currencyList = GetCurrenciesList();
+
+            if ((CurrenciesDollarValue == null || CurrenciesDollarValue.Count == 0) && currencyList.Count == 0)
+            {
+                return _missingData;
+            }
 
             currencyList.ForEach(c =>
             {
@@ -52,6 +63,11 @@ namespace Domain.Models
 
         public string GetTimeZone()
         {
+            if (Timezones == null || Timezones.Count == 0)
+            {
+                return _missingData;
+            }
+
             var dictionary = TimeUtils.TimeToUTCDictionary(Timezones);
 
             return TimeUtils.DictionaryToString(dictionary);
---
+++ Tests/IpToLocationMock.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Models;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    static public class IpToLocationMock
+    {
+        static public IpToLocationModel GetIpToLocationModelMock()
+        {
+            return new IpToLocationModel
+            {
+                Country_name = "Argentina",
+                Country_code = "ARG",
+                Currencies = new List<IpToLocationModel.CurrencyModel>()
+                {
+                    new IpToLocationModel.CurrencyModel()
+                    {
+                        Code = "ARS"
+                    }
+                },
+                CurrenciesDollarValue = new List<string>()
+                {
+                    "200"
+                },
+                Latitude = "-34",
+                Longitude = "-58",
+                Timezones = new List<string>()
+                {
+                    "UTC-3", "UTC-2", "UTC"
+                }
+            };
+        }
+    }
+}
