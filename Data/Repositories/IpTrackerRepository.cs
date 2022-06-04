9242e8c2b99437aea8547da0c5ef8e8376acf9a5
Germán <germanmoreyra94@gmail.com>
2022-06-04 19:10:25 -0300

    Refactor repo

--- Data/Repositories/IpTrackerRepository.cs
+++ Data/Repositories/IpTrackerRepository.cs
@@ -16,6 +16,7 @@ namespace Data.Repositories
         private static readonly string _apiKey = "z6MKj5YtvvtUnORpxxvASLIUvzfeo4Aq";
         private static readonly string _urlIpToLocation = "https://api.apilayer.com/ip_to_location/";
         private static readonly string _urlFixer = "https://api.apilayer.com/fixer/";
+        private static readonly string _patternKey = KeyUtils.APP + KeyUtils.STATISTIC + "*";
 
         private readonly IDistributedCache _memoryCache;
 
@@ -70,53 +71,62 @@ namespace Data.Repositories
 
         public async void AddStatistic(IpInfoModel ipInfoModel)
         {
+            if (ipInfoModel is not null 
+                && !string.IsNullOrWhiteSpace(ipInfoModel.DistanceToBA) 
+                && !string.IsNullOrWhiteSpace(ipInfoModel.Country))
+            {
+                var statsKey = GetStatiscticKey(ipInfoModel);
+                var cached = await _memoryCache.GetRecordAsync<StatisticModel>(statsKey);
+
+                if (cached is null)
+                {
+                    var statistic = new StatisticModel()
+                    {
+
+                        CountryName = ipInfoModel.Country,
+                        DistanceToBaKms = StringUtils.StringKmsToInt(ipInfoModel.DistanceToBA)
+                    };
+
+                    await _memoryCache.SetRecordAsync(statsKey, statistic, TimeSpan.FromHours(24));
+                }
+                else
+                {
+                    await _memoryCache.RemoveAsync(statsKey);
+
+                    cached.InvocationCounter++;
+
+                    await _memoryCache.SetRecordAsync(statsKey, cached, TimeSpan.FromHours(24));
+                }
+            }
+        }
+
+        private string GetStatiscticKey(IpInfoModel ipInfoModel)
+        {
             var countryName = ipInfoModel.Country;
             var distanceToBa = ipInfoModel.DistanceToBA;
-
             var distanceParsed = StringUtils.StringKmsToInt(distanceToBa);
-
             var statsKey = KeyUtils.STATISTIC + countryName + distanceParsed.ToString();
 
-            var cached = await _memoryCache.GetRecordAsync<StatisticModel>(statsKey);
-
-            if (cached is null)
-            {
-                var statistic = new StatisticModel(){
-
-                    CountryName = countryName,
-                    DistanceToBaKms = distanceParsed
-                };
-
-                await _memoryCache.SetRecordAsync(statsKey, statistic, TimeSpan.FromHours(24));
-            }
-            else
-            {
-                await _memoryCache.RemoveAsync(statsKey);
-
-                cached.InvocationCounter++;
-
-                await _memoryCache.SetRecordAsync(statsKey, cached, TimeSpan.FromHours(24));
-            }
+            return statsKey;
         }
 
         public async Task<List<StatisticModel>> ReturnAllStatistics()
         {
-            var patternKey = KeyUtils.APP + KeyUtils.STATISTIC + "*";
-            var allValue = new List<StatisticModel>();
+            var alltatistics = new List<StatisticModel>();
             using (var redis = ConnectionMultiplexer.Connect("localhost:6379,allowAdmin=true"))
             {
                 IDatabase db = redis.GetDatabase(0);
-                var keys = redis.GetServer("localhost:6379").Keys(pattern: patternKey, pageSize: 1000);
+                var keys = redis.GetServer("localhost:6379").Keys(pattern: _patternKey, pageSize: 1000);
                 var keysArr = keys.Select(key => (string)key).ToArray();
                 foreach (var key in keysArr)
                 {
-                    var keyy = key.Replace(KeyUtils.APP, "");
-                    var result = await _memoryCache.GetRecordAsync<StatisticModel>(keyy);
-                    allValue.Add(result);
+                    var statsKey = key.Replace(KeyUtils.APP, "");
+                    var result = await _memoryCache.GetRecordAsync<StatisticModel>(statsKey);
+                    alltatistics.Add(result);
                 }
             }
 
-            return allValue;
+            return alltatistics;
         }
 
         public List<StatisticModel> ReturnMaxMinStatistics(List<StatisticModel> statisticModels)
@@ -127,20 +137,32 @@ namespace Data.Repositories
             }
 
             var minMaxStats = new List<StatisticModel>();
-
-            var maxDistanceValue = statisticModels.Max(d => d.DistanceToBaKms);
-            var minDistanceValue = statisticModels.Min(d => d.DistanceToBaKms);
-
-            var maxInvocation = statisticModels.Where(x => x.DistanceToBaKms == maxDistanceValue).Max(x => x.InvocationCounter);
-            var minInvocation = statisticModels.Where(x => x.DistanceToBaKms == minDistanceValue).Max(x => x.InvocationCounter);
-
-            var maxStatisticModel = statisticModels.Where(x => x.DistanceToBaKms == maxDistanceValue && x.InvocationCounter == maxInvocation).First();
-            var minStatisticModel = statisticModels.Where(x => x.DistanceToBaKms == minDistanceValue && x.InvocationCounter == minInvocation).First();
+            var maxStatisticModel = MinMaxStatisticModel(statisticModels, "Max");
+            var minStatisticModel = MinMaxStatisticModel(statisticModels, "Min");
 
             minMaxStats.Add(maxStatisticModel);
             minMaxStats.Add(minStatisticModel);
 
             return minMaxStats;
+        }
+
+        private StatisticModel MinMaxStatisticModel(List<StatisticModel> statisticModels, string method)
+        {
+            var distanceValue = 0;
+
+            if (method.ToUpper() == "MAX")
+            {
+                distanceValue = statisticModels.Max(d => d.DistanceToBaKms);
+            }
+            else if (method.ToUpper() == "MIN")
+            {
+                distanceValue = statisticModels.Min(d => d.DistanceToBaKms);
+            }
+            
+            var invocationValue = statisticModels.Where(x => x.DistanceToBaKms == distanceValue).Max(x => x.InvocationCounter);
+            var statisticModel = statisticModels.Where(x => x.DistanceToBaKms == distanceValue && x.InvocationCounter == invocationValue).First();
+
+            return statisticModel;
         }
 
         private async Task<RestResponse> GetResponseFromAPI(string parameter, string url)
