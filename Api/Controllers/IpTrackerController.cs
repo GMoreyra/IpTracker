40fbb34bc7934f6295cefe870772cb6bf6a78d72
Germán <germanmoreyra94@gmail.com>
2024-02-24 19:49:20 +0100

    Refactor: controller - Add ctor validation. - Add Produces and Consumes. - Add summaries. - Add ProducesResponseType. - Rework returns.

--- Api/Controllers/IpTrackerController.cs
+++ Api/Controllers/IpTrackerController.cs
@@ -1,66 +1,72 @@
 ﻿using Application.Interfaces;
+using Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 using Utils;
 
+using Media = System.Net.Mime.MediaTypeNames;
+
 namespace Api.Controllers;
 
+/// <summary>
+/// Controller for tracking IP addresses.
+/// </summary>
 [ApiController]
 [Route("[controller]")]
+[Produces(Media.Application.Json)]
+[Consumes(Media.Application.Json)]
 public class IpTrackerController : ControllerBase
 {
     private readonly IIpTrackerService _service;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="IpTrackerController"/> class.
+    /// </summary>
+    /// <param name="service">The service to be used for IP tracking.</param>
+    /// <exception cref="ArgumentNullException">Thrown when the provided service is null.</exception>
     public IpTrackerController(IIpTrackerService service)
     {
-        _service = service;
+        _service = service ?? throw new ArgumentNullException(nameof(service));
     }
 
     /// <summary>
-    /// Returns the ip info based on the ip address
+    /// Gets the geolocation of the specified IP address.
     /// </summary>
-    /// <param name="ipAddress">ip address</param>
-    /// <returns></returns>
+    /// <param name="ipAddress">The IP address to get the geolocation for.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains the action result.</returns>
     [HttpGet("{ipAddress}")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<IActionResult> GetGeolocationAsync(string ipAddress)
+    [ProducesResponseType(typeof(List<StatisticModel>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<IpInfoModel>> GetGeolocationAsync(string ipAddress)
     {
         if (!StringUtils.ValidateString(ipAddress))
         {
-            return BadRequest();
+            return BadRequest(ipAddress);
         }
 
         var response = await _service.GetIpInfo(ipAddress);
 
-        if (response is null)
-        {
-            return NotFound();
-        }
-
-        return Ok(response);
+        return response is null ? NotFound(response) : Ok(response);
     }
 
     /// <summary>
-    /// Returns the statistics
+    /// Gets the statistics of the tracked IP addresses.
     /// </summary>
+    /// <returns>A task that represents the asynchronous operation. The task result contains the action result.</returns>
     [HttpGet("statistic")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<IActionResult> GetStatisticsAsync()
+    [ProducesResponseType(typeof(List<StatisticModel>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<List<StatisticModel>>> GetStatisticsAsync()
     {
         var response = await _service.GetStatistics();
 
-        if (response is null)
-        {
-            return NotFound();
-        }
-
-        return Ok(response);
+        return response is null ? NotFound(response) : Ok(response);
     }
 
     /// <summary>
-    /// Returns the average distance
+    /// Gets the average distance of the tracked IP addresses.
     /// </summary>
+    /// <returns>A task that represents the asynchronous operation. The task result contains the action result.</returns>
     [HttpGet("average")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -68,11 +74,6 @@ public class IpTrackerController : ControllerBase
     {
         var response = await _service.GetAverageDistance();
 
-        if (response is null)
-        {
-            return NotFound();
-        }
-
-        return Ok(response);
+        return response is null ? NotFound(response) : Ok(response);
     }
 }
